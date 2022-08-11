c73704f907863deae871844bb85fd7e60a72ee92
Vanya <vanya.danilov.95@bk.ru>
2022-08-11 23:22:48 +0500

    task15

---
+++ task(15)/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число дня недели, где Понедельник = 1, Вторник = 2 и т.д ");
+int num = int.Parse(Console.ReadLine());
+if(num % 7 == 0 || num % 6 == 0 ){
+    Console.WriteLine("Выходной");
+}
+else{
+    Console.WriteLine("Не выходной");
+}
