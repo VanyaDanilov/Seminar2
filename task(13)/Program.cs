73b0dad9cf34032d1cbadc8ad4d42289eb37af5c
Vanya <vanya.danilov.95@bk.ru>
2022-08-11 23:01:00 +0500

    task13

---
+++ task(13)/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите  число ");
+int number = int.Parse(Console.ReadLine());
+int num = number % 1000;
+int num1 = num / 100;
+Console.WriteLine ($"Число: { num1} из {number}");
+
+
